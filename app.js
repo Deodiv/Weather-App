eefa364d1685048691838db5f68a6fe8f26f7592
Divine Sofowora <deodiv7@gmail.com>
2023-03-23 12:19:33 +0100

    ...

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-fetch("http://api.weatherapi.com/v1/current.json?key=13857fdb8d274be8b94223700231903&q=London&aqi=no")
-    .then(res => res.json()) // parse response as JSON
+document.querySelector('button').addEventListener('click', getWeather);
+
+function getWeather() {
+  let weather = document.querySelector('input').value;
+
+  fetch(`http://api.weatherapi.com/v1.json?key=13857fdb8d274be8b94223700231903&q=${weather}&aqi=no`)
+    .then(res => res.json())
     .then(data => {
-      console.log(data)
+      console.log(data.location.name);
+      document.querySelector('.degree').innerText = data.location.name;
     })
     .catch(err => {
-        console.log(`error ${err}`)
+      console.log(`error ${err}`);
     });
+}
