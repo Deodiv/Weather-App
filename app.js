c44be481bed5c7db301769b2f5c89b78d9e814b5
Divine Sofowora <deodiv7@gmail.com>
2023-03-28 14:29:47 +0100

    Appended location to the DOM

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-document.querySelector('button').addEventListener('click' , getWeather)
+document.querySelector('button').addEventListener('click', searchCity)
 
-function getWeather (){
+function searchCity (){
+  let nameOfCity = document.querySelector('input').value 
 
-  fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6b9e0121dfac89ca6d12494666507be9')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    document.querySelector('.location').innerText = data.name
-  })
-  .catch(err => {
-    console.log(`error ${err}`);
-  });
+fetch(`http://api.weatherapi.com/v1/current.json?key=13857fdb8d274be8b94223700231903&q=${nameOfCity}&aqi=no`)
+.then(res => res.json())
+.then(data => {
+  console.log(data);  
+  document.querySelector('.location').innerText = data.location.name
+})
+.catch(err => {
+  console.log(`error ${err}`);
+});
+
+
 }
+
+
+
+
+
+
+
+
+
+// document.querySelector('button').addEventListener('click', search)
+
+// function search(){
+//   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`)
+// .then(res => res.json())
+// .then(data => {
+//   console.log(data);
+//   document.querySelector('.location').innerText = data.name
+// })
+// .catch(err => {
+//   console.log(`error ${err}`);
+// });
+// }
