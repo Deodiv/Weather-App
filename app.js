32af7ac2903eb67f808d4afcfcf1e1e5f08979cd
Divine Sofowora <deodiv7@gmail.com>
2023-03-28 07:05:36 +0100

    initial fetch made

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-document.querySelector('button').addEventListener('click', getWeather);
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6b9e0121dfac89ca6d12494666507be9')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(`error ${err}`);
+  });
 
-function getWeather() {
-  let weather = document.querySelector('input').value;
 
-  fetch(`http://api.weatherapi.com/v1.json?key=13857fdb8d274be8b94223700231903&q=${weather}&aqi=no`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.location.name);
-      document.querySelector('.degree').innerText = data.location.name;
-    })
-    .catch(err => {
-      console.log(`error ${err}`);
-    });
-}
