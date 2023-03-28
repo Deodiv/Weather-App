16761927ca1ee2e715457acaee330b8cf21eb221
Divine Sofowora <deodiv7@gmail.com>
2023-03-28 07:15:00 +0100

    Added a click event that will enable me to search.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6b9e0121dfac89ca6d12494666507be9')
+document.querySelector('button').addEventListener('click' , getWeather)
+
+function getWeather (){
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6b9e0121dfac89ca6d12494666507be9')
   .then(res => res.json())
   .then(data => {
     console.log(data);
+    document.querySelector('.location').innerText = data.name
   })
   .catch(err => {
     console.log(`error ${err}`);
   });
-
-
+}
